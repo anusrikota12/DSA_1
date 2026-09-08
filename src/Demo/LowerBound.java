25cfe2f5f8ca8939df169976a46dd39b1aed02da
anusrikota12 <anusrikota68@gmail.com>
2026-09-08 13:59:38 +0530

    LowerBound

---
+++ src/Demo/LowerBound.java
@@ -0,0 +1,22 @@
+package Demo;
+
+public class LowerBound {
+	static int lowerBound(int[] arr,int target) {
+		int left=0;
+		int right=arr.length;
+		while(left<right) {
+			int mid=left+(right-left)/2;
+			if(arr[mid]>=target) {
+				right=mid;
+			} else {
+				left=mid+1;
+			}
+		}
+		return left;
+	}
+	public static void main(String[] args) {
+		int[] arr= {10,20,20,20,30,40,50};
+		int target=10;
+		System.out.println(lowerBound(arr,target));
+	}
+}
